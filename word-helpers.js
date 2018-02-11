e13a7a6e7a981795eac9484652df7d1711a861c5
rjbultitude <richard.bultitude@gmail.com>
2018-02-11 22:49:39 +0000

    rework comma full stop fn

--- word-helpers.js
+++ word-helpers.js
@@ -16,7 +16,11 @@ module.exports = {
         return str.replace(this.conjunctionsRegX);
     },
     wordsUntilCommaStop: function wordsUntilCommaStop(str) {
-        return str.split(',')[0] || str.split('.')[0];
+        const stringWithCommaStop = str.split(',|.')[0];
+        if (stringWithCommaStop.length > 1) {
+            return stringWithCommaStop;
+        }
+        return false;
     },
     firstThreeWords: function firstThreeWords(str) {
         return str.split(' ').slice(0, 3).join(' ');
@@ -24,6 +28,7 @@ module.exports = {
     truncateStrings: function truncateStrings(strs) {
         return strs.map((str) => {
             const wordsCommaStop = this.wordsUntilCommaStop(str);
+            console.log('wordsCommaStop', wordsCommaStop);
             if (wordsCommaStop) {
                 return wordsCommaStop;
             }
