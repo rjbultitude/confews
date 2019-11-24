bba04d3d443ecf3ae20e9541aeb97908353398a5
rjbultitude <richard.bultitude@gmail.com>
2019-11-24 13:02:28 +0000

    restructure for Google Cloud Functions from firebase. update selectors for news sources. fix logging issues

--- app.local.js
+++ app.local.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const app = require('./app');
+const app = require('./functions/confews/index');
 const port = process.env.PORT || 3000;
-app.listen(port, () => 
+app.listen(port, () =>
   console.log(`Server is listening on port ${port}.`)
 );
